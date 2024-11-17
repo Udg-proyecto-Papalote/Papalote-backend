cd4ee9921a04bc84e8c788fc59c1c4ab8430efba
Ximena Vega <ximena.garcia7482@alumnos.udg.mx>
2024-11-17 16:48:26 -0600

    Refactor app.py: remove user registration and login functionality, update CORS configuration

--- src/app.py
+++ src/app.py
@@ -1,103 +1,13 @@
 from flask import Flask, request, jsonify, Response
-from flask_pymongo import PyMongo
-from werkzeug.security import generate_password_hash, check_password_hash
-from bson import json_util
-from config import MONGODB_URI
-import os
+from flask_cors import CORS
 from diagnostico import procesar_audio_y_generar_json
 import json
-from flask_cors import CORS
 
 # Create the Flask app
 app = Flask(__name__)
 
-# Configure the database
-app.config['MONGO_URI'] = MONGODB_URI
-
-# Create the PyMongo instance
-mongo = PyMongo(app)
-CORS(app, resources={r"/*": {"origins": "http://localhost:5173"}}, methods=["GET", "POST", "OPTIONS"])
-
-# Register a new user
-@app.route('/users', methods=['POST'])
-def create_user():
-    # Receive the data
-    email = request.json['email']
-    password = request.json['password']
-    name = request.json['name']
-
-    response = None
-
-    if email and password and name:
-        # Hash the password
-        hashed_password = generate_password_hash(password)
-
-        # Check if the user already exists
-        user = mongo.db.users.find_one({'email': email})
-
-        if user:
-            response = jsonify({'message': 'User already exists'})
-            response.status_code = 400
-            return response
-
-        # Insert the data
-        id = mongo.db.users.insert_one({
-            'email': email,
-            'password': hashed_password,
-            'name': name
-        })
-
-        response = jsonify({
-            'message': 'User created successfully',
-            'id': str(id)
-        })
-        response.status_code = 201
-    
-    return response
-    
-# Login
-@app.route('/login', methods=['POST'])
-def login():
-    # Receive the data
-    email = request.json['email']
-    password = request.json['password']
-
-    response = None
-
-    if email and password:
-        # Get the user from the database
-        user = mongo.db.users.find_one({'email': email})
-
-        if user:
-            # Check the password
-            if check_password_hash(user['password'], password):
-                # Add the user ID and name to the headers
-                response.headers['user_id'] = str(user['_id'])
-                response.headers['name'] = user['name']
-                response = jsonify({'message': 'Login successful'})
-                response.status_code = 200
-                
-
-            else:
-                response = jsonify({'message': 'Wrong password'})
-                response.status_code = 400
-        else:
-            response = jsonify({'message': 'User not found'})
-            response.status_code = 400
-
-    return response
-
-# Define the route
-@app.route('/users', methods=['GET'])
-def get_users():
-    # Get the data from the database
-    users = mongo.db.users.find()
-
-    # Convert the data to JSON
-    response = json_util.dumps(users)
-
-    # Return the response
-    return Response(response, mimetype='application/json')
+# Configure CORS
+CORS(app, resources={r"/*": {"origins": "*"}})s
 
 # Ruta para diagnóstico
 @app.route('/diagnostico', methods=['POST'])
